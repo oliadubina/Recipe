9766675da4ca9c4bf40a7f2586fb7b9f98627fc0
Olha <olhadub08@gmail.com>
2025-05-21 13:05:59 +0300

    Added test for recipe

--- recipe/tests.py
+++ recipe/tests.py
@@ -6,3 +6,19 @@ class CategoryModelTest(TestCase):
         category = Category.objects.create(name='Dessert')
         self.assertEqual(category.name, 'Dessert')
         self.assertEqual(str(category), 'Dessert')
+class RecipeModelTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name='Dessert')
+
+    def test_create_recipe(self):
+        recipe = Recipe.objects.create(
+            title='Chocolate Cake',
+            description='Delicious chocolate cake',
+            instructions='Mix, bake, enjoy.',
+            ingredients='Chocolate, flour, sugar, eggs',
+            category=self.category
+        )
+        self.assertEqual(recipe.title, 'Chocolate Cake')
+        self.assertEqual(recipe.category.name, 'Dessert')
+        self.assertIn('chocolate', recipe.description.lower())
+        self.assertEqual(str(recipe), 'Chocolate Cake')
